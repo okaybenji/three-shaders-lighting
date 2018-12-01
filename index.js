ea62e6dd9881ba4a9164589ede5a291a9923050c
Benji Kay <okaybenji@gmail.com>
2018-11-30 20:50:14 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// Based on this tutorial: https://gamedevelopment.tutsplus.com/tutorials/a-beginners-guide-to-coding-graphics-shaders-part-2--cms-24111
+
+/** Set up Three.js **/
+// First we initialize the scene and our camera
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// We create the WebGL renderer and add it to the document
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+/** Configure our shader **/
+// Prepare the texture
+THREE.ImageUtils.crossOrigin = ''; // Allows us to load an external image
+const tex = THREE.ImageUtils.loadTexture( 'https://media.giphy.com/media/oYtVHSxngR3lC/giphy.gif' );
+
+// Get the shader code
+const fragmentShader = document.getElementById('fragShader').innerHTML;
+
+// Configure the uniforms
+const startTime = Date.now();
+const uniforms = {
+  resolution: {
+    type: 'v2',
+    value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+  },
+  time: {
+    type: 'f',
+    value: 0.,
+  },
+  texture: {
+    type: 't',
+    value: tex,
+  },
+  mousePos: {
+    type:'v2',
+    value: new THREE.Vector2(),
+  },
+};
+
+/** Set up the scene **/
+const geometry = new THREE.PlaneBufferGeometry( 10, 10 );
+const material = new THREE.ShaderMaterial( {fragmentShader, uniforms} );
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+mesh.position.z = -1.25; // Shift the mesh back so we can see it
+
+/** Start animating **/
+const render = () => {
+  // Update our shader's uniforms.
+  uniforms.time.value = Date.now() - startTime;
+
+  // Animate the scene.
+  requestAnimationFrame( render );
+  renderer.render( scene, camera );
+};
+
+render();
+
+// Listen for events to update uniforms.
+window.onresize = function(event){
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  uniforms.resolution.value.x = window.innerWidth;
+  uniforms.resolution.value.y = window.innerHeight;
+}
+
+document.onmousemove = function(event){
+    //Update the light source to follow our mouse
+    uniforms.mousePos.value.x = event.clientX;
+    uniforms.mousePos.value.y = event.clientY;
+}
